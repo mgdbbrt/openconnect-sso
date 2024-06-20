162a183faba71c2db79ac461deab064aa9a7cc86
Max-Georg Debbert <113188948+mgdbbrt@users.noreply.github.com>
2024-06-20 12:50:03 +0200

    chore: deduplicate flake inputs

--- flake.nix
+++ flake.nix
@@ -1,11 +1,18 @@
 {
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.systems.follows = "systems";
+    };
     poetry2nix = {
       url = "github:nix-community/poetry2nix/2024.5.939250";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+        systems.follows = "systems";
+      };
     };
+    systems.url = "github:nix-systems/default";
   };
 
   outputs = { self, flake-utils, nixpkgs, ... }@inputs: (flake-utils.lib.eachDefaultSystem (
