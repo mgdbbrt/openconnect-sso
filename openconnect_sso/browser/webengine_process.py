3fd551c3d1a310d0b9f520b22bc707d490c791a5
Max-Georg Debbert <113188948+mgdbbrt@users.noreply.github.com>
2025-09-02 11:51:18 +0200

    fix: replace pkg_resources

--- openconnect_sso/browser/webengine_process.py
+++ openconnect_sso/browser/webengine_process.py
@@ -6,7 +6,7 @@ import sys
 from urllib.parse import urlparse
 
 import attr
-import pkg_resources
+import importlib.resources
 import structlog
 
 from PyQt6.QtCore import QUrl, QTimer, pyqtSlot, Qt
@@ -157,7 +157,9 @@ class WebBrowser(QWebEngineView):
             return self._popupWindow.view()
 
     def authenticate_at(self, url, credentials):
-        script_source = pkg_resources.resource_string(__name__, "user.js").decode()
+        script_source = (
+            importlib.resources.files(__name__).joinpath("user.js").read_text()
+        )
         script = QWebEngineScript()
         script.setInjectionPoint(QWebEngineScript.InjectionPoint.DocumentCreation)
         script.setWorldId(QWebEngineScript.ScriptWorldId.ApplicationWorld)
